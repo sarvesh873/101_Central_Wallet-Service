22f950423954761dcc8a4c17024a6afdc35f6787
sarvesh873 <sarveshvarade873@gmail.com>
2025-12-06 23:06:25 +0530

    Added enhance README with Dockerize and Kafka Config

---
+++ src/main/java/com/central/wallet_service/kafka/UserEventConsumer.java
@@ -0,0 +1,9 @@
+package com.central.wallet_service.kafka;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+public class UserEventConsumer {
+}
